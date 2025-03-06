f2edb63959949776d8cd8bfdd702ebd1a6e2cc8f
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-06 19:39:03 +0530

    Create Main.java and adding a public class Main

---
+++ Main.java
@@ -0,0 +1,15 @@
+// Name: Ameya Tipnis
+// PRN: 23070126012
+// Batch: 2023-27 (A1)
+
+// Main.java
+
+// Importing Scanner for User Input
+import java.util.Scanner;
+
+// Defining a public class named Main
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Creating Scanner object for taking user-input
+    }
+}
