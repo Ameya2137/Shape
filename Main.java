d07ce23043a5fc06980ec8be7c286575d56c9b0d
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-06 19:42:34 +0530

    Update Main.java by adding a code for menu-driven program by using do-while loop

--- Main.java
+++ Main.java
@@ -11,5 +11,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Creating Scanner object for taking user-input
+        int choice; // Declaring an integer variable to store user input
+
+        // Using do-while for making a menu-driven program
+        do {
+            System.out.println("\nChoose a Shape:");
+            System.out.println("1. Circle");
+            System.out.println("2. Rectangle");
+            System.out.println("3. Square");
+            System.out.println("4. Sphere");
+            System.out.println("5. Cylinder");
+            System.out.println("6. Equilateral Pyramid (Square Base)");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt(); // Reads a number 0-6 from user and stores it in choice
+        }
     }
 }
