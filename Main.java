29a7b919e0972c4d4fe686cd4a2e853dd0b9ab80
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-06 19:47:35 +0530

    Update Main.java by adding code for calculating the mensuration for a Circle

--- Main.java
+++ Main.java
@@ -15,6 +15,7 @@ public class Main {
 
         // Using do-while for making a menu-driven program
         do {
+            // Prompting the uer to enter a number between 0-6
             System.out.println("\nChoose a Shape:");
             System.out.println("1. Circle");
             System.out.println("2. Rectangle");
@@ -24,7 +25,18 @@ public class Main {
             System.out.println("6. Equilateral Pyramid (Square Base)");
             System.out.println("0. Exit");
             System.out.print("Enter your choice: ");
-            choice = scanner.nextInt(); // Reads a number 0-6 from user and stores it in choice
+            choice = scanner.nextInt(); // Reads the prompted number from user and stores it in choice
+
+            // Switch statement selects a case based on user's input
+            switch (choice) {
+                case 1:
+                    // Prompting the user to enter the radius
+                    System.out.print("Enter radius: ");
+                    Circle circle = new Circle(scanner.nextDouble());
+                    System.out.println("Area: " + circle.calculateArea());
+                    System.out.println("Perimeter: " + circle.calculatePerimeter());
+                    break;
+            }
         }
     }
 }
