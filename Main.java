076598e66f3a913401d94d83fb253cf8a39bc59f
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-06 19:51:09 +0530

    Update Main.java by adding code for calculating the mensuration for a Square

--- Main.java
+++ Main.java
@@ -30,22 +30,29 @@ public class Main {
             // Switch statement selects a case based on user's input
             switch (choice) {
                 case 1:
-                    // Prompting the user to enter the radius
+                    // Prompting the user to enter the radius for a Circle
                     System.out.print("Enter radius: ");
                     Circle circle = new Circle(scanner.nextDouble());
                     System.out.println("Area: " + circle.calculateArea());
                     System.out.println("Perimeter: " + circle.calculatePerimeter());
                     break;
                 case 2:
-                    // Prompting the user to enter the length
+                    // Prompting the user to enter the length for a Rectangle
                     System.out.print("Enter length: ");
                     double length = scanner.nextDouble();
-                    // Prompting the user to enter the width
+                    // Prompting the user to enter the width for a Rectangle
                     System.out.print("Enter width: ");
                     double width = scanner.nextDouble();
                     Rectangle rectangle = new Rectangle(length, width);
                     System.out.println("Area: " + rectangle.calculateArea());
                     System.out.println("Perimeter: " + rectangle.calculatePerimeter());
+                    break;
+                case 3:
+                    // Prompting the user to enter the length for a Square
+                    System.out.print("Enter side length: ");
+                    Square square = new Square(scanner.nextDouble());
+                    System.out.println("Area: " + square.calculateArea());
+                    System.out.println("Perimeter: " + square.calculatePerimeter());
                     break;
             }
         }
