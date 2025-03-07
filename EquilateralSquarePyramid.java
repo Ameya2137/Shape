e58771864c84a126ea3ed1ff38353978faeef742
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-07 09:51:49 +0530

    Update EquilateralSquarePyramid.java by adding code for calculating for area, perimeter and volume

--- EquilateralSquarePyramid.java
+++ EquilateralSquarePyramid.java
@@ -10,4 +10,20 @@ class EquilateralPyramid extends Shape implements Volume {
         this.baseSide = baseSide;
         this.height = height;
     }
+
+    // Method to calculate area of a EquilateralPyramid
+    public double calculateArea() {
+        double slantHeight = Math.sqrt((baseSide / 2) * (baseSide / 2) + height * height);
+        return (baseSide * baseSide) + 2 * baseSide * slantHeight;
+    }
+
+    // Method to calculate perimeter of a EquilateralPyramid
+    public double calculatePerimeter() {
+        return 4 * baseSide;
+    }
+
+    // Method to calculate volume of a EquilateralPyramid
+    public double calculateVolume() {
+        return (1.0 / 3.0) * (baseSide * baseSide) * height;
+    }
 }
