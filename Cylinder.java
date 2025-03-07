acb4917b2fddbadd1f09c8fc7ba14875808ebc99
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-07 09:45:49 +0530

    Update Cylinder.java by adding code for calculating area, perimeter and volume

--- Cylinder.java
+++ Cylinder.java
@@ -10,4 +10,19 @@ class Cylinder extends Shape implements Volume {
         this.radius = radius;
         this.height = height;
     }
+
+    // Method to calculate area of a Cylinder
+    public double calculateArea() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    // Method to calculate perimeter of a Cylinder
+    public double calculatePerimeter() {
+        return 0; // No perimeter for a cylinder
+    }
+
+    // Method to calculate volume of a Cylinder
+    public double calculateVolume() {
+        return Math.PI * radius * radius * height;
+    }
 }
